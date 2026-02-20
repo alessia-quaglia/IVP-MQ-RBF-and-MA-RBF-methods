551f5a02f6a92b83f9099d1f5fb1d5de2fc4b4ea
alessia-quaglia <alessia.quaglia929@edu.unito.it>
2026-02-20 11:06:14 +0100

    Update AB2_classic.m

--- Stability region/AB2_classic.m
+++ Stability region/AB2_classic.m
@@ -15,6 +15,7 @@ z = (r.^2 - r) ./ (1.5*r - 0.5);
 plot(real(z), imag(z),'Color', 'b', 'LineWidth', 2);
 grid on;
 axis equal;
-xlabel('Re(h \lambda)');
-ylabel('Im(h \lambda)');
+xlabel('Re(h\lambda)');
+ylabel('Im(h\lambda)');
+
 
