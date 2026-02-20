208a44f4704ded4fd709ccf187087cd784fb95bb
alessia-quaglia <alessia.quaglia929@edu.unito.it>
2026-02-20 17:46:03 +0100

    Update script_tab4_3_fig4_3.m

--- script_tab4_3_fig4_3.m
+++ script_tab4_3_fig4_3.m
@@ -5,6 +5,9 @@
 % corresponding MQ-RBF and MA-RBF modifications applied to the IVP: 
 % u'(t) = -u^2, u(0) = 1
 
+addpath('Classic methods IVP')
+addpath('RBF methods IVP')
+
 f = @(t,u) -u.^2;
 u_esatta = @(t) 1./(1 + t); 
 a = 0;
@@ -65,4 +68,5 @@ set(gca, 'XTick', N, 'XTickLabel', string(N));
 axis([10 400 10^(-9) 10^(-2)])
 title('Global errors for $u'' = -u^2$', 'Interpreter', 'latex');
 axis square; grid on; box on;
+
 hold off;
