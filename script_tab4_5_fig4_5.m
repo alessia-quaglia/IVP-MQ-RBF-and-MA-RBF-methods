50519f80dc99b88222b4e1673a87104f3b3e412e
alessia-quaglia <alessia.quaglia929@edu.unito.it>
2026-02-22 17:37:59 +0100

    Update script_tab4_5_fig4_5.m

--- script_tab4_5_fig4_5.m
+++ script_tab4_5_fig4_5.m
@@ -7,7 +7,7 @@ addpath('Classic methods IVP')
 addpath('RBF methods IVP')
 
 f = @(t,u) (2.*t.^2-u)/(t.^2.*u-t);
-u_esatta = @(t) 1./t + sqrt(1./(t.^2)+4.*t-4);
+u_exact = @(t) 1./t + sqrt(1./(t.^2)+4.*t-4);
 a = 1;
 b = 3;
 u0 = 2;
@@ -19,9 +19,9 @@ err_GAmidpoint = zeros(size(N));
 
 for i = 1:length(N)
     n = N(i);
-    err_midpoint(i) = midpoint(f, u_esatta, a, b, u0, n);
-    err_MQmidpoint(i) = RBF_midpoint(f, u_esatta, a, b, u0, n, 1);
-    err_GAmidpoint(i) = RBF_midpoint(f, u_esatta, a, b, u0, n, 2);
+    err_midpoint(i) = midpoint(f, u_exact, a, b, u0, n);
+    err_MQmidpoint(i) = RBF_midpoint(f, u_exact, a, b, u0, n, 1);
+    err_GAmidpoint(i) = RBF_midpoint(f, u_exact, a, b, u0, n, 2);
 end
 
 ord_midpoint = [NaN, log2(err_midpoint(1:end-1)./err_midpoint(2:end))];
@@ -31,7 +31,7 @@ ord_GAmidpoint = [NaN, log2(err_GAmidpoint(1:end-1)./err_GAmidpoint(2:end))];
 %% Table generation
 met_name = {'Midpoint', 'MA-RBF Midpoint', 'GA-RBF Midpoint'};
 met_err = {err_midpoint, err_MQmidpoint, err_GAmidpoint};
-met_ord  = {ord_midpoint, ord_MQmidpoint, ord_GAmidpoint};
+met_ord = {ord_midpoint, ord_MQmidpoint, ord_GAmidpoint};
 table_latex(met_name, N, met_err, met_ord);
 
 %% Figure generation
@@ -40,9 +40,9 @@ hold on;
 slope_N = [10, 320];
 slope = 1e-2 * (slope_N/slope_N(1)).^-3; 
 g_s = loglog(slope_N, slope, 'k--', 'LineWidth', 1.2);
-g_midpoint   = loglog(N, err_midpoint, 'b-o', 'LineWidth', 1, 'MarkerSize', 7);
-g_MAmidpoint  = loglog(N, err_MQmidpoint,   'k-s', 'LineWidth', 1, 'MarkerSize', 7);
-g_GAmidpoint = loglog(N, err_GAmidpoint,   'r-*', 'LineWidth', 1, 'MarkerSize', 7);
+g_midpoint = loglog(N, err_midpoint, 'b-o', 'LineWidth', 1, 'MarkerSize', 7);
+g_MAmidpoint = loglog(N, err_MQmidpoint, 'k-s', 'LineWidth', 1, 'MarkerSize', 7);
+g_GAmidpoint = loglog(N, err_GAmidpoint, 'r-*', 'LineWidth', 1, 'MarkerSize', 7);
 legend([g_midpoint, g_MAmidpoint, g_GAmidpoint, g_s], ...
        {'Midpoint', 'MA-RBF Midpoint', 'GA-RBF Midpoint', 'Slope -3'}, ...
        'Location', 'southwest', 'FontSize', 9);
@@ -53,3 +53,4 @@ axis([10 400 10^(-7) 10^(1)])
 title('Global errors for $u'' = \frac{2t^2-u}{t^2u-t}$', 'Interpreter', 'latex');
 axis square; grid on; box on;
 hold off;
+
